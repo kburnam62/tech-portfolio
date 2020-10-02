2e6c9ee5da93f6f3a27cb80db66ea0b21e2b3b93
kburnam62 <72276755+kburnam62@users.noreply.github.com>
2020-10-02 15:53:00 -0400

    Updated tagline

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -13,7 +13,7 @@ const users = [];
 
 const siteConfig = {
     title: 'Kaitlin Burnam', // Title for your website.
-    tagline: 'Entrepreneur, Programmer, Creator',
+    tagline: 'Creative Person',
     profilePhoto: 'img/profile.png',
     url: 'https://kburnam62.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
